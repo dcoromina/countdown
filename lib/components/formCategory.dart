9aa6ebe8f4f4e98700700b0cb3a790ab41a341dc
Myoo-David <d@myhealp.com>
2024-09-26 11:43:32 +0200

    update

--- lib/components/formCategory.dart
+++ lib/components/formCategory.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyCategory extends StatefulWidget {
   const MyCategory({super.key});
@@ -9,12 +11,35 @@ class MyCategory extends StatefulWidget {
 
 class _MyCategoryState extends State<MyCategory> {
   bool showTravel = false;
+  bool showSports = false;
+
+  int count = 0;
+
+  void ShowTravelSearch() {
+    setState(() {
+      showTravel == false ? showTravel = true : showTravel = false;
+    });
+  }
+
+  void ShowSportModal() {
+    setState(() {
+      showSports == false ? showSports = true : showSports = false;
+    });
+  }
+
+  void ButtonTest() {
+    setState(() {
+      showTravel;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    bool showField = false;
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(
+        const SizedBox(
             child: Padding(
           padding: EdgeInsets.all(10.0),
           child: Text(
@@ -33,21 +58,79 @@ class _MyCategoryState extends State<MyCategory> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   SizedBox(
-                    width: 70,
-                    height: 70,
-                    child: Card(
-                      elevation: 5,
-                      child: Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Center(
-                            child: Icon(
-                          Icons.sports_basketball_outlined,
+                    width: 65,
+                    height: 65,
+                    child: ElevatedButton(
+                      style: const ButtonStyle(
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)))),
+                          alignment: Alignment.center,
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.white),
+                          elevation: MaterialStatePropertyAll(5)),
+                      onPressed: () => {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return Dialog(
+                              shape: RoundedRectangleBorder(
+                                  side: const BorderSide(
+                                      width: 1, color: Colors.white38),
+                                  borderRadius: BorderRadius.circular(15)),
+                              elevation: 16,
+                              child: Container(
+                                color: Colors.transparent,
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  child: SizedBox(
+                                    child: Row(
+                                      children: [
+                                        ElevatedButton(
+                                            onPressed: ButtonTest,
+                                            child:
+                                                Icon(Icons.sports_basketball)),
+                                        ElevatedButton(
+                                            onPressed: ButtonTest,
+                                            child: Icon(Icons.sports_soccer)),
+                                        ElevatedButton(
+                                            onPressed: ButtonTest,
+                                            child: Icon(Icons.sports_golf)),
+                                        ElevatedButton(
+                                            onPressed: ButtonTest,
+                                            child: Icon(Icons.sports_baseball)),
+                                        ElevatedButton(
+                                            onPressed: ButtonTest,
+                                            child: Icon(Icons.sports_cricket)),
+                                        ElevatedButton(
+                                            onPressed: ButtonTest,
+                                            child: Icon(Icons.sports_football)),
+                                        ElevatedButton(
+                                            onPressed: ButtonTest,
+                                            child: Icon(Icons.sports_hockey)),
+                                        ElevatedButton(
+                                            onPressed: ButtonTest,
+                                            child:
+                                                Icon(Icons.sports_volleyball)),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        )
+                      },
+                      child: const Center(
+                        child: Icon(
+                          Icons.sports_basketball,
                           size: 40,
-                        )),
+                        ),
                       ),
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 70,
                     height: 70,
                     child: Card(
@@ -66,7 +149,7 @@ class _MyCategoryState extends State<MyCategory> {
                     width: 65,
                     height: 65,
                     child: ElevatedButton(
-                      style: ButtonStyle(
+                      style: const ButtonStyle(
                           shape: MaterialStatePropertyAll(
                               RoundedRectangleBorder(
                                   borderRadius:
@@ -75,8 +158,8 @@ class _MyCategoryState extends State<MyCategory> {
                           backgroundColor:
                               MaterialStatePropertyAll(Colors.white),
                           elevation: MaterialStatePropertyAll(5)),
-                      onPressed: null,
-                      child: Center(
+                      onPressed: () => {ShowTravelSearch()},
+                      child: const Center(
                         child: Icon(
                           Icons.flight,
                           size: 40,
@@ -84,7 +167,8 @@ class _MyCategoryState extends State<MyCategory> {
                       ),
                     ),
                   ),
-                  SizedBox(
+
+                  const SizedBox(
                     width: 70,
                     height: 70,
                     child: Card(
@@ -99,7 +183,7 @@ class _MyCategoryState extends State<MyCategory> {
                       ),
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 70,
                     height: 70,
                     child: Card(
@@ -114,7 +198,7 @@ class _MyCategoryState extends State<MyCategory> {
                       ),
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 70,
                     height: 70,
                     child: Card(
@@ -135,6 +219,46 @@ class _MyCategoryState extends State<MyCategory> {
             ),
           ),
         ),
+        Visibility(
+          visible: showTravel,
+          child: Container(
+            width: 150,
+            height: 50,
+            color: Colors.amber,
+          ),
+        ),
+        Visibility(
+          visible: showSports,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: SizedBox(
+              child: Row(
+                children: [
+                  ElevatedButton(
+                      onPressed: ButtonTest,
+                      child: Icon(Icons.sports_basketball)),
+                  ElevatedButton(
+                      onPressed: ButtonTest, child: Icon(Icons.sports_soccer)),
+                  ElevatedButton(
+                      onPressed: ButtonTest, child: Icon(Icons.sports_golf)),
+                  ElevatedButton(
+                      onPressed: ButtonTest,
+                      child: Icon(Icons.sports_baseball)),
+                  ElevatedButton(
+                      onPressed: ButtonTest, child: Icon(Icons.sports_cricket)),
+                  ElevatedButton(
+                      onPressed: ButtonTest,
+                      child: Icon(Icons.sports_football)),
+                  ElevatedButton(
+                      onPressed: ButtonTest, child: Icon(Icons.sports_hockey)),
+                  ElevatedButton(
+                      onPressed: ButtonTest,
+                      child: Icon(Icons.sports_volleyball)),
+                ],
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
--- lib/components/timer.dart
+++ lib/components/timer.dart
@@ -1,7 +1,7 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'dart:async';
+
+import '../pages/card_detail.dart';
 
 class CountdownTimerScreen extends StatefulWidget {
   final DateTime targetDate; // Specify your target date here
@@ -38,29 +38,31 @@ class _CountdownTimerScreenState extends State<CountdownTimerScreen> {
   }
 
   String _formatTimeDays(Duration duration) {
-    var days = duration.inDays.truncate();
+    var days = duration.inDays;
+    int remainingMonths = 31 - DateTime.now().day;
 
-    return '$days';
+    return '$remainingMonths';
   }
 
   String _formatTimeMonths(Duration duration) {
-    final months = duration.inDays / 30;
+    var months = duration.inDays;
+    double monthstrue = months.toDouble() / 30;
+    months = monthstrue.toInt();
 
-    return ' $months';
+    return '$months';
   }
 
   String _formatTimeYears(Duration duration) {
-    final months = duration.inDays / 30;
-    print(duration.inDays / 30);
-    var years = months / 12.truncate();
-    return '$years years';
+    var years = duration.inDays / 30 / 12;
+    var tyears = years.truncate();
+    return '$tyears';
   }
 
   String _formatTimeHours(Duration duration) {
     final hours = duration.inHours % 24;
     final seconds = duration.inSeconds % 60;
 
-    return ' $hours hours $seconds seconds';
+    return '$hours';
   }
 
 /*     final years = months / 12;
@@ -77,7 +79,282 @@ class _CountdownTimerScreenState extends State<CountdownTimerScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double halfParentHeight = constraints.maxHeight / 2;
+
+        return Dismissible(
+          behavior: HitTestBehavior.translucent,
+          confirmDismiss: (direction) async {
+            if (direction != DismissDirection.startToEnd) {
+              /* -------------- Delete card on swipe -----------*/
+              return Future(() => showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('Are you sure?'),
+                        content: const Text(
+                          'Are you sure you want to delete this countdown? Theres no going back after this.',
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              textStyle: Theme.of(context).textTheme.labelLarge,
+                            ),
+                            child: const Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              textStyle: Theme.of(context).textTheme.labelLarge,
+                            ),
+                            child: const Text('Delete'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  ));
+            } else {
+              /* ----------- Edit card on swipe -------*/
+              return Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const CardDetail()));
+            }
+          },
+          secondaryBackground: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15), color: Colors.red),
+            child: const Align(
+                alignment: AlignmentDirectional.centerEnd,
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Icon(Icons.delete_forever, size: 75),
+                )),
+          ),
+          background: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15), color: Colors.blue),
+            child: const Align(
+                alignment: AlignmentDirectional.centerStart,
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Icon(Icons.edit, size: 75),
+                )),
+          ),
+          key: const Key("dissmisible"),
+          child: Container(
+            height: MediaQuery.of(context).size.height / 4.5,
+            width: MediaQuery.of(context).size.width / 1.05,
+            decoration: BoxDecoration(
+                boxShadow: const <BoxShadow>[
+                  BoxShadow(
+                    offset: Offset(2.0, 2.0),
+                    blurRadius: 10.0,
+                    color: Color.fromARGB(157, 0, 0, 0),
+                  ),
+                ],
+                image: const DecorationImage(
+                    image: NetworkImage(
+                        "https://dcoromina.s-ul.eu/icons/Brvmqy4f"),
+                    fit: BoxFit.fitWidth,
+                    colorFilter:
+                        ColorFilter.mode(Colors.black12, BlendMode.darken),
+                    opacity: 100),
+                borderRadius: BorderRadius.circular(15)),
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(15),
+                              topRight: Radius.circular(15)),
+                          color: Colors.black26),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                _formatTimeMonths(_remainingTime),
+                                style: const TextStyle(
+                                    shadows: <Shadow>[
+                                      Shadow(
+                                        offset: Offset(2.0, 2.0),
+                                        blurRadius: 10.0,
+                                        color: Color.fromARGB(157, 0, 0, 0),
+                                      ),
+                                    ],
+                                    height: 0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 60),
+                              ),
+                              const Text(
+                                "Months",
+                                style: TextStyle(
+                                    shadows: <Shadow>[
+                                      Shadow(
+                                        offset: Offset(2.0, 2.0),
+                                        blurRadius: 10.0,
+                                        color: Color.fromARGB(157, 0, 0, 0),
+                                      ),
+                                    ],
+                                    height: 0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
+                              )
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                _formatTimeDays(_remainingTime),
+                                style: TextStyle(
+                                    shadows: <Shadow>[
+                                      Shadow(
+                                        offset: Offset(2.0, 2.0),
+                                        blurRadius: 10.0,
+                                        color: Color.fromARGB(157, 0, 0, 0),
+                                      ),
+                                    ],
+                                    height: 0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 60),
+                              ),
+                              Text(
+                                "Days",
+                                style: TextStyle(
+                                    shadows: <Shadow>[
+                                      Shadow(
+                                        offset: Offset(2.0, 2.0),
+                                        blurRadius: 10.0,
+                                        color: Color.fromARGB(157, 0, 0, 0),
+                                      ),
+                                    ],
+                                    height: 0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
+                              )
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                _formatTimeHours(_remainingTime),
+                                style: TextStyle(
+                                    shadows: <Shadow>[
+                                      Shadow(
+                                        offset: Offset(2.0, 2.0),
+                                        blurRadius: 10.0,
+                                        color: Color.fromARGB(157, 0, 0, 0),
+                                      ),
+                                    ],
+                                    height: 0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 60),
+                              ),
+                              Text(
+                                "Hours",
+                                style: TextStyle(
+                                    shadows: <Shadow>[
+                                      Shadow(
+                                        offset: Offset(2.0, 2.0),
+                                        blurRadius: 10.0,
+                                        color: Color.fromARGB(157, 0, 0, 0),
+                                      ),
+                                    ],
+                                    height: 0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
+                              )
+                            ],
+                          )
+                        ],
+                      )),
+                ),
+                const Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          shadows: <Shadow>[
+                            Shadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 10.0,
+                              color: Color.fromARGB(157, 0, 0, 0),
+                            ),
+                          ],
+                          Icons.flight_takeoff_outlined,
+                          color: Colors.white,
+                          size: 50,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Wrap(
+                              children: [
+                                Text(
+                                  "Travel to brazil",
+                                  style: TextStyle(
+                                      shadows: <Shadow>[
+                                        Shadow(
+                                          offset: Offset(2.0, 2.0),
+                                          blurRadius: 10.0,
+                                          color: Color.fromARGB(157, 0, 0, 0),
+                                        ),
+                                      ],
+                                      height: 0,
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              "25/5/2024",
+                              style: TextStyle(
+                                  height: 0,
+                                  shadows: <Shadow>[
+                                    Shadow(
+                                      offset: Offset(2.0, 2.0),
+                                      blurRadius: 10.0,
+                                      color: Color.fromARGB(157, 0, 0, 0),
+                                    ),
+                                  ],
+                                  color: Colors.white),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+
+    /* Scaffold(
       body: Column(
         children: [
           Center(
@@ -100,7 +377,7 @@ class _CountdownTimerScreenState extends State<CountdownTimerScreen> {
           ),
         ],
       ),
-    );
+    ); */
   }
 }
 
